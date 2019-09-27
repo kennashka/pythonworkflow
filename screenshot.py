ad0445439a35d096a4cfb48e8e3450c460aaad0d
Kennashka DeSilva <mail@kennashka.com>
2019-09-27 00:55:43 -0400

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -3,7 +3,7 @@ import time
 
 x = 1
 
-while x < 4:
-pyautogui.screenshot('/Users/macuser/Desktop/screenshot/image'+str(x)+'.png')
-x+=1
-time.sleep(2)
+while x < 4: # number of screenshots 
+    pyautogui.screenshot('/Users/macuser/Desktop/screenshot/image'+str(x)+'.png') #location to save the file
+    x+=1 #while loop condition
+    time.sleep(2) # every two seconds 
